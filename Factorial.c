280dd8752705d2caf846746218e12c7900e912c5
Akshat Dodhiya <dodhiyaakshat@gmail.com>
2023-08-25 22:24:41 -0400

    Improve readability

--- Factorial.c
+++ Factorial.c
@@ -1,22 +1,23 @@
-//
-// Created by Akshat on 8/25/2023.
-//
+/*
+Created by Akshat on 8/25/2023.
+*/
 
 #include <stdio.h>
 
 int main()
 {
     int num, factorial = 1;
-    int isValid = 0;
+    int isValid = 0;  // Variable that checks validity of user's input
 
-    printf("**********Factorial Program***********\n\n");
+    printf("********** Factorial Number Program ***********\n\n");
 
     do {
         printf("Enter the number --> ");
         fflush(stdin);
-        isValid = scanf("%d", &num);
+        isValid = scanf("%d", &num);  // scanf() returns 1 (true) if the desired value is entered by the user, and 0(false) if input received is undesired
     } while(!isValid);
 
+    // Find factorial and store in variable
     for(int i=2; i<=num; i++)
     {
         factorial = factorial * i;
@@ -24,7 +25,7 @@ int main()
 
     printf("The factorial of %d is %d", num, factorial);
 
-    fflush(stdin);
-    getchar();
+    fflush(stdin);  // Clears the output buffer
+    getchar();  // To prevent window from abruptly closing without the user being able to see the output
     return 0;
 }
